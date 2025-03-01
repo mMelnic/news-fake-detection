f12939ae1a5e67981f7abc54cd01ffe350e713ea
Mihaela Melnic <moldovamelnik@gmail.com>
2025-03-01 22:57:06 +0100

    Combine fetching, storing and caching from NewsAPI using everything enpoint

--- news_aggregator/news/views.py
+++ news_aggregator/news/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from news.fetchers.news_api_fetcher import NewsApiFetcher
+from news.services.article_service import ArticleService
+from news.utils.storage import get_cached_result, cache_result
+
+def fetch_and_store_articles(query, language=None):
+    fetcher = NewsApiFetcher()
+    article_service = ArticleService()
+
+    cached_result = get_cached_result(query, language)
+    if cached_result:
+        return cached_result
+
+    articles = fetcher.fetch_articles(query, language)
+    if articles:
+        article_service.store_articles(articles, query)
+        cache_result(query, language, articles)
+    return articles
