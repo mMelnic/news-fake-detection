c3e96ce4b1ff91d56c380fd34f146434c711959f
Mihaela Melnic <moldovamelnik@gmail.com>
2025-03-04 00:47:27 +0100

    Rewrite News  API request using base class methods

--- news_aggregator/news/fetchers/news_api_fetcher.py
+++ news_aggregator/news/fetchers/news_api_fetcher.py
@@ -1,24 +1,20 @@
-import requests
-from urllib.parse import quote
 from datetime import datetime, timedelta
 import os
+from .base_fetcher import BaseFetcher
+from .fetcher_interface import FetcherInterface
 
 NEWS_API_URL = "https://newsapi.org/v2/everything"
 NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 MAX_PAGES = 5
 VALID_LANGUAGES = {'ar', 'de', 'en', 'es', 'fr', 'he', 'it', 'nl', 'no', 'pt', 'ru', 'sv', 'ud', 'zh'}
 
-class NewsApiFetcher:
+class NewsApiFetcher(BaseFetcher, FetcherInterface):
     def __init__(self):
         self.api_key = NEWS_API_KEY
 
-    def fetch_articles(self, query, language=None):
+    def fetch_articles(self, query, language=None, country=None):
         query = self._validate_and_format_query(query)
         language = self._validate_language(language)
-        articles = self._fetch_from_api(query, language)
-        return articles
-
-    def _fetch_from_api(self, query, language):
         one_week_ago = (datetime.now() - timedelta(days=7)).isoformat()
 
         params = {
@@ -31,20 +27,14 @@ class NewsApiFetcher:
         if language:
             params["language"] = language
 
-        response = requests.get(NEWS_API_URL, params=params)
-        if response.status_code != 200:
-            raise Exception(f"Error {response.status_code}: {response.json()}")
+        articles = []
+        try:
+            response_data = self._send_request(NEWS_API_URL, params)
+            articles = response_data.get("articles", [])
+        except Exception as e:
+            self.logger.error(f"Failed to fetch articles: {e}")
 
-        data = response.json()
-        articles = data.get("articles", [])
         return articles
-
-    def _validate_and_format_query(self, query):
-        if not query or not isinstance(query, str):
-            raise ValueError("Query must be a non-empty string.")
-        if len(query) > 500:
-            raise ValueError("Query must not exceed 500 characters.")
-        return quote(query.strip())
 
     def _validate_language(self, language):
         if language and language not in VALID_LANGUAGES:
