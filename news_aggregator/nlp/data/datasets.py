97efd86949342d3bcf058b53c36f45dbc9073f05
Mihaela Melnic <moldovamelnik@gmail.com>
2025-03-09 20:52:37 +0100

    Add method to load data from a csv file

--- news_aggregator/nlp/data/datasets.py
+++ news_aggregator/nlp/data/datasets.py
@@ -13,3 +13,23 @@ class DatasetLoader:
             data = [json.loads(line) for line in file]
         df = pd.DataFrame(data)
         return df[required_columns]
+    
+    def load_csv(self, file_path: str, required_columns: list[str] = None) -> pd.DataFrame:
+        """
+        Loads data from a CSV file and extracts the required columns.
+        :param file_path: Path to the CSV file.
+        :param required_columns: List of columns to extract. If None, all columns will be loaded.
+        :return: A pandas DataFrame containing the extracted data.
+        """
+        try:
+            if required_columns:
+                data = pd.read_csv(file_path, usecols=required_columns)
+            else:
+                data = pd.read_csv(file_path)
+            return data
+        except FileNotFoundError:
+            print(f"Error: File not found at path {file_path}")
+            return pd.DataFrame()
+        except ValueError as ve:
+            print(f"Error: {ve}")
+            return pd.DataFrame()
