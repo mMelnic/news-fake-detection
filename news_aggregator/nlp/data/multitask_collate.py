d1d7919d1603bc6b3085e70f4f8112aa184bffbf
Mihaela Melnic <moldovamelnik@gmail.com>
2025-04-12 17:06:18 +0200

    Fix error

--- news_aggregator/nlp/data/multitask_collate.py
+++ news_aggregator/nlp/data/multitask_collate.py
@@ -1,4 +1,5 @@
 import torch
+import ast
 
 def multitask_collate_fn(batch):
     """
@@ -8,8 +9,8 @@ def multitask_collate_fn(batch):
     batch_dict = {"input_ids": [], "attention_mask": [], "labels": [], "tasks": []}
     
     for sample in batch:
-        batch_dict["input_ids"].append(torch.tensor(sample["input_ids"]))
-        batch_dict["attention_mask"].append(torch.tensor(sample["attention_mask"]))
+        batch_dict["input_ids"].append(torch.tensor(ast.literal_eval(sample["input_ids"]), dtype=torch.long))
+        batch_dict["attention_mask"].append(torch.tensor(ast.literal_eval(sample["attention_mask"]), dtype=torch.long))
         batch_dict["labels"].append(sample["label"])
         batch_dict["tasks"].append(sample["task"])
     
