7233416625d11085f2add45fbc7436637d176fdf
Mihaela Melnic <moldovamelnik@gmail.com>
2025-04-14 00:25:12 +0200

    Separate head for topic classification with increased drop out

--- news_aggregator/nlp/models/heads.py
+++ news_aggregator/nlp/models/heads.py
@@ -10,7 +10,15 @@ class TaskHeadFactory:
         :param num_classes: The number of output classes for the task.
         :return: A PyTorch module for the task-specific head.
         """
-        if task_name in ["topic_classification", "fake_news_detection", "sentiment_analysis"]:
+        if task_name == "topic_classification":
+            return nn.Sequential(
+                nn.Dropout(0.5),  # Higher dropout for topic head
+                nn.Linear(hidden_size, hidden_size//2),
+                nn.LayerNorm(hidden_size//2), # Layer normalization for stability
+                nn.ReLU(),
+                nn.Linear(hidden_size//2, num_classes)
+            )
+        elif task_name in ["fake_news_detection", "sentiment_analysis"]:
             return nn.Sequential(
                 nn.Dropout(p=0.1),
                 nn.Linear(hidden_size, hidden_size // 2),
