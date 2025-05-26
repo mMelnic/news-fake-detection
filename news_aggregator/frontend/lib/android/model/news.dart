513e8821147937f08f4723a340923df01160a4b5
Mihaela Melnic <moldovamelnik@gmail.com>
2025-05-26 12:38:00 +0200

    Updated news model to follow article structure

--- news_aggregator/frontend/lib/android/model/news.dart
+++ news_aggregator/frontend/lib/android/model/news.dart
@@ -1,25 +1,44 @@
 class News {
+  final String id;
   final String title;
-  final String photo;
-  final String description;
-  final String date;
+  final String content;
+  final String image;
   final String author;
+  final DateTime date;
+  final String sourceUrl;
+  final String sourceName;
+  final String category;
+  final bool isFake;
+  final String sentiment;
 
   News({
+    required this.id,
     required this.title,
-    required this.photo,
-    required this.description,
-    required this.date,
+    required this.content,
+    required this.image,
     required this.author,
+    required this.date,
+    required this.sourceUrl,
+    required this.sourceName,
+    required this.category,
+    required this.isFake,
+    required this.sentiment,
   });
 
   factory News.fromMap(Map<String, dynamic> map) {
     return News(
+      id: map['id']?.toString() ?? '',
       title: map['title']?.toString() ?? '',
-      photo: map['photo']?.toString() ?? '',
-      description: map['description']?.toString() ?? '',
-      date: map['date']?.toString() ?? '',
-      author: map['author']?.toString() ?? '',
+      content: map['content']?.toString() ?? '',
+      image: map['image_url']?.toString() ?? '',
+      author: map['author']?.toString() ?? 'Unknown',
+      date:
+          DateTime.tryParse(map['published_date'] ?? '') ?? DateTime.now(),
+      sourceUrl: map['url']?.toString() ?? '',
+      sourceName: map['source']?.toString() ?? '',
+      category: map['categories']?.toString() ?? '',
+      isFake: map['is_fake'] ?? false,
+      sentiment: map['sentiment']?.toString() ?? 'neutral',
     );
   }
 }
